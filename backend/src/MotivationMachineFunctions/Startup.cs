ca05ad2a0f33aca07088e257236a542abed1b3ee
Joshua <joshuaquarryhouse@gmail.com>
2025-02-22 13:18:44 +0000

    Added terraform and basic Lambda function

---
+++ backend/src/MotivationMachineFunctions/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MotivationMachineFunctions;
+
+[LambdaStartup]
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+}
